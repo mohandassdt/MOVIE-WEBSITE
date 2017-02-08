2625e3525f90e3b8db953eefcb907a748c132f65
mohandassdt <vijimohandass.s@gmail.com>
2017-02-08 17:51:15 +0530

    server commit

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 
 var logger = require('morgan');
 var express = require('express');
-var routes = require('./routes/movie-crud');
-var routes = require('./routes/city-crud');
-var routes = require('./routes/theater-crud');
-var routes = require('./routes/showtime-crud');
+var app = express();
 var bodyParser=require('body-parser');
 
-var app = express();
+var routes = require('./routes/movie-crud');
+var routesCity = require('./routes/city-crud');
+var routesTheater = require('./routes/theater-crud');
+var routesShow = require('./routes/showtime-crud');
+var routesMapping = require('./routes/mapping-crud');
+
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.use('/movie', routes)
-app.use('/city', routes)
-app.use('/theater', routes)
-app.use('/show', routes)
+app.use('/movie', routes);
+app.use('/cty', routesCity);
+app.use('/theater', routesTheater);
+app.use('/showt', routesShow);
+app.use('/map', routesMapping);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
 
+
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var dbHost = 'mongodb://localhost:27017/test';
+mongoose.connect(dbHost);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("Connected to DB");
+});
 // Only load this middleware in dev mode (important).
 if (app.get('env') === 'development') {
   var webpackMiddleware = require("webpack-dev-middleware");
